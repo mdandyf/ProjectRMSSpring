80bbc6552d97fd786f8ed42482d2aeb5ac1abd35
Dandy <mochamad.dandy@gmail.com>
2019-01-09 10:29:36 +0700

    injecting configuration to read resources data (css, js and jpeg)

--- src/main/java/com/main/config/WebSecurityConfiguration.java
+++ src/main/java/com/main/config/WebSecurityConfiguration.java
@@ -31,7 +31,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
+                .antMatchers("/", "/home", "/public/**").permitAll()
+                .antMatchers("/private/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
