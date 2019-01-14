a1f649d001eb1cd223f6c9357628926d846db755
Dandy <mochamad.dandy@gmail.com>
2019-01-14 17:53:38 +0700

    changing react-js related to the security and login page

--- react/src/index.js
+++ react/src/index.js
@@ -1,29 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import Home from './App-login.jsx';
-import Login from './App-login.jsx';
-import List from './App-list.jsx';
-import Form from './App-form.jsx';
-import * as serviceWorker from './serviceWorker';
+import Routing from './jsx/App-router.jsx'
 
-ReactDOM.render((
-
-    <Router>
-        <div>
-            <Route exact path="/" render={() => (
-                <Redirect to="/home"/>
-            )}/>
-            <Route path="/login" component={Login} />
-            <Route path="/home" component={Home} />
-            <Route path="/list" component={List} />
-            <Route path="/form" component={Form} />
-        </div>
-    </Router>
-
-), document.getElementById('app'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Routing/>, document.getElementById('app'));
