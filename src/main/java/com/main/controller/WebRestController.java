1b2a6d01ca374e7437d39823e5ac150f9ccf4b02
Dandy <mochamad.dandy@gmail.com>
2019-01-15 17:44:43 +0700

    updating for activating the roles

--- src/main/java/com/main/controller/WebRestController.java
+++ src/main/java/com/main/controller/WebRestController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.web.bind.annotation.*;
@@ -29,53 +30,53 @@ public class WebRestController {
     @Autowired
     UserDetailsService userDetailsService;
 
-    //@PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
+    @PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
     @RequestMapping(value = "/list/user/{id}", method = RequestMethod.GET)
     public ResponseEntity<Object> getUserById(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAuthority('READ_PRIVILEGE')")
+    @PreAuthorize("hasAuthority('READ_PRIVILEGE')")
     @RequestMapping(value = "/list/users", method = RequestMethod.GET)
     public ResponseEntity<Object> getListUsers() {
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
+    @PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
     @RequestMapping(value = "/list/details", method = RequestMethod.GET)
     public ResponseEntity<Object> getListDetails() {
         return new ResponseEntity<>(userService.getUserDetails(), HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
+    @PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
     @RequestMapping(value = "/list/user/roles/{id}", method = RequestMethod.GET)
     public ResponseEntity<Object> getUserRole(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.findUserRole(id), HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
+    @PreAuthorize("hasAuthority('EDIT_PRIVILEGE')")
     @RequestMapping(value = "/list/user/privileges/{id}", method = RequestMethod.GET)
     public ResponseEntity<Object> getUserPrivileges(@PathVariable("id") Long id) {
         return new ResponseEntity<>(userService.findUserPrivileges(id), HttpStatus.OK);
     }
 
-    //@PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
+    @PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
     @RequestMapping(value = "/save-user", method = RequestMethod.POST)
     public ResponseEntity<Object> saveUser(@RequestBody User user) {
         userService.saveUser(user, user.getId());
         return new ResponseEntity<>("User has been saved", HttpStatus.CREATED);
     }
 
-    //@PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
+    @PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
     @RequestMapping(value = "/save-role", method = RequestMethod.POST)
     public ResponseEntity<Object> saveRole(@RequestBody Role role) {
         roleService.saveRole(role);
         return new ResponseEntity<>("Role has been saved", HttpStatus.CREATED);
     }
 
-    //@PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
+    @PreAuthorize("hasAnyAuthority('NEW_PRIVILEGE')")
     @RequestMapping(value = "/save-privilege", method = RequestMethod.POST)
-    public ResponseEntity<Object> saveRole(@RequestBody Privilege privilege) {
+    public ResponseEntity<Object> savePrivilege(@RequestBody Privilege privilege) {
         privilegeService.savePrivilege(privilege);
         return new ResponseEntity<>("Privilege has been saved", HttpStatus.CREATED);
     }
